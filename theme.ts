159dad9f4812ae89d56e13ec8ce6e64fd2b65c16
vinuch <vincentedeh42@gmail.com>
2025-11-18 10:01:53 -0800

    chore: uninstall tailwindcss and setup chakra themeing

---
+++ theme.ts
@@ -0,0 +1,54 @@
+import { createSystem, defaultConfig, defineConfig } from "@chakra-ui/react"
+
+const config = defineConfig({
+    globalCss: {
+        "html, body": {
+            margin: 0,
+            padding: 0,
+        },
+    },
+    theme: {
+        breakpoints: {
+            sm: "320px",
+            md: "768px",
+            lg: "960px",
+            xl: "1200px",
+        },
+        tokens: {
+            colors: {
+                red: {
+                    value: "#EE0F0F"
+                },
+                primary: {
+                    value: "#75C5C1"
+                },
+                secondary: {
+                    value: "#41245F"
+                },
+            },
+            fonts: {
+                body: { value: "var(--font-jakarta-sans), sans-serif" },
+                heading: { value: "var(--font-jakarta-sans), sans-serif" },
+            }
+        },
+        // semanticTokens: {
+        //     colors: {
+        //         danger: { value: "{colors.red}" },
+        //         secondary: {
+        //             value: "#41245F"
+        //         },
+        //         primary: {
+        //             value: "colors.primary"
+        //         },
+        //     },
+        // },
+        keyframes: {
+            spin: {
+                from: { transform: "rotate(0deg)" },
+                to: { transform: "rotate(360deg)" },
+            },
+        },
+    },
+})
+
+export default createSystem(defaultConfig, config)
