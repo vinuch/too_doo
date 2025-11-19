de3adb7dcb62f5226ecf5425e8fb98be02655df2
vinuch <vincentedeh42@gmail.com>
2025-11-18 17:31:25 -0800

    feat: main content area base ui Headers WIP

---
+++ components/Sidebar/index.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import { Box, IconButton, Collapsible, Stack, Button, Text, VStack } from "@chakra-ui/react";
+import { useState } from "react";
+import { LuChevronDown, LuChevronLeft, LuChevronRight } from "react-icons/lu";
+
+export default function Sidebar() {
+    const [isOpen, setIsOpen] = useState(false);
+
+    return (
+        <Box></Box>
+    );
+}
